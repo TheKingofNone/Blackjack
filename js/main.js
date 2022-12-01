284c021410f38e5c1274ace17636c5b51a224663
TheKingofNone <akingpercussion@gmail.com>
2022-12-01 14:55:17 -0500

    Deck of Cards

---
+++ js/main.js
@@ -0,0 +1,220 @@
+/*----- constants -----*/
+const newDeck = [
+    {
+        name : 'hA',
+        weight1 : 11,
+        weight2 : 1,
+    },
+    {
+        name : 'dA',
+        weight1 : 11,
+        weight2 : 1,
+    },
+    {
+        name : 'sA',
+        weight1 : 11,
+        weight2 : 1,
+    },
+    {
+        name : 'cA',
+        weight1 : 11,
+        weight2 : 1,
+    },
+    {
+        name : 'hK',
+        weight : 10,
+    },
+    {
+        name : 'dK',
+        weight : 10,
+    },
+    {
+        name : 'sK',
+        weight : 10,
+    },
+    {
+        name : 'cK',
+        weight : 10,
+    },
+    {
+        name : 'hQ',
+        weight : 10,
+    },
+    {
+        name : 'dQ',
+        weight : 10,
+    },
+    {
+        name : 'sQ',
+        weight : 10,
+    },
+    {
+        name : 'hJ',
+        weight : 10,
+    },
+    {
+        name : 'dJ',
+        weight : 10,
+    },
+    {
+        name : 'sJ',
+        weight : 10,
+    },
+    {
+        name : 'cJ',
+        weight : 10,
+    },
+    {
+        name : 'cQ',
+        weight : 10,
+    },
+    {
+        name : 'h10',
+        weight : 10,
+    },
+    {
+        name : 'd10',
+        weight : 10,
+    },
+    {
+        name : 's10',
+        weight : 10,
+    },
+    {
+        name : 'c10',
+        weight : 10,
+    },
+    {
+        name : 'h09',
+        weight : 9,
+    },
+    {
+        name : 'd09',
+        weight : 9,
+    },
+    {
+        name : 's09',
+        weight : 9,
+    },
+    {
+        name : 'c09',
+        weight : 9,
+    },
+    {
+        name : 'd08',
+        weight : 8,
+    },
+    {
+        name : 'h08',
+        weight : 8,
+    },
+    {
+        name : 's08',
+        weight : 8,
+    },
+    {
+        name : 'c08',
+        weight : 8,
+    },
+    {
+        name : 'h07',
+        weight : 7,
+    },
+    {
+        name : 'd07',
+        weight : 7,
+    },
+    {
+        name : 's07',
+        weight : 7,
+    },
+    {
+        name : 'c07',
+        weight : 7,
+    },
+    {
+        name : 'h06',
+        weight : 6,
+    },
+    {
+        name : 'd06',
+        weight : 6,
+    },
+    {
+        name : 's06',
+        weight : 6,
+    },
+    {
+        name : 'c06',
+        weight : 6,
+    },
+    {
+        name : 'd05',
+        weight : 5,
+    },
+    {
+        name : 'h05',
+        weight : 5,
+    },
+    {
+        name : 's05',
+        weight : 5,
+    },
+    {
+        name : 'c05',
+        weight : 5,
+    },
+    {
+        name : 'd04',
+        weight : 4,
+    },
+    {
+        name : 'h04',
+        weight : 4,
+    },
+    {
+        name : 's04',
+        weight : 4,
+    },
+    {
+        name : 'c04',
+        weight : 4,
+    },
+    {
+        name : 'h03',
+        weight : 3,
+    },
+    {
+        name : 'd03',
+        weight : 3,
+    },
+    {
+        name : 's03',
+        weight : 9,
+    },
+    {
+        name : 'c03',
+        weight : 3,
+    },
+    {
+        name : 'ch02',
+        weight : 2,
+    },
+    {
+        name : 'd02',
+        weight : 2,
+    },
+    {
+        name : 's02',
+        weight : 2,
+    },
+    {
+        name : 'c02',
+        weight : 2,
+    },
+]
+console.log(newDeck.length);
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
