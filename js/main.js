a3131e6c908569b3b1a45ab87500bcf5af04dc77
TheKingofNone <akingpercussion@gmail.com>
2022-12-06 11:08:54 -0500

    Javasccript functionality

--- js/main.js
+++ js/main.js
@@ -218,6 +218,67 @@ let playerHand = [];
 let houseHand = [];
 /*----- cached element references -----*/
 /*----- event listeners -----*/
+
+$(document).ready(function(){
+    $("#bttn1").click(function(){
+        $("#bttn1, .blackjack").hide();
+        $("#bttn4").removeClass("hidden").addClass("container");
+    });
+});
+$("#bttn4").click(function(){
+    $("#bttn4").removeClass("container").addClass("hidden");
+    grabDeck = [...newDeck];
+    playerHand = [];
+    houseHand = [];
+    console.log(grabDeck);
+    startingDeal();
+});
+$("#bttn2").click(function(){
+     deal(playerHand);
+     console.log(playerHand);
+     let sum1 = addHand(playerHand);
+     console.log(sum1);
+     if ((sum1 > 21) === true){
+        hideHitStand();
+        $("#bttn5").removeClass("hidden").addClass("container");
+        console.log(sum1 + ", You Bust");
+    };
+});
+$("#bttn3").click(function(){
+    let sum1= addHand(playerHand);
+    let sum2 = addHand(houseHand);
+    while (sum2 < 17){
+        deal(houseHand);
+        sum2 = addHand(houseHand);
+    }
+    console.log(sum2);
+    if ((sum2 > 21) === true){
+        hideHitStand();
+        $("#bttn5").removeClass("hidden").addClass("container");
+        console.log("house busts you win");
+     }
+     else{
+        hideHitStand();
+        console.log("player- " + sum1 + " house- " + sum2);
+        if ((sum1 > sum2) === true){
+           console.log("you win");
+        }
+        else if((sum2 > sum1) === true){
+            console.log("you lose");
+        }
+        else{
+            console.log("Push");
+        };
+        $("#bttn5").removeClass("hidden").addClass("container");
+    }
+});
+$("#bttn5").click(function(){
+    // grabDeck = null;
+    // playerHand = null;
+    // houseHand = null;
+    $("#bttn5").removeClass("container").addClass("hidden");
+    $("#bttn4").removeClass("hidden").addClass("container");
+})
 /*----- functions -----*/
 /* function which switches the last object in the array with a random object, then removes the last object from the function*/
 function shuffle(array){
@@ -233,6 +294,36 @@ function deal(hand){
     let card = grabDeck.pop();
     hand.push(card);
 }
+function startingDeal(){
+    $("#bttn2, #bttn3").removeClass("hidden").addClass("container");
+        shuffle(grabDeck);
+        deal(playerHand);
+        deal(houseHand);
+        deal(playerHand);
+        deal(houseHand);
+        console.log(playerHand);
+        console.log(houseHand);
+        let sum1 = addHand(playerHand);
+        let sum2 = addHand(houseHand);
+        if (sum1 === 21 && sum2 != 21){
+            hideHitStand();
+            $("#bttn5, #blackjackWin").removeClass("hidden").addClass("container");
+            console.log("Blackjack-- You Win!");
+        }
+        else if (sum1 === 21 && sum2 === 21){
+            hideHitStand();
+            $("#bttn5, #push").removeClass("hidden").addClass("container");
+            console.log("Player and House Made Blackjack, Push");
+        }
+        else if (sum1 != 21 && sum2 === 21){
+            hideHitStand();
+            $("#bttn5, #houseBlackjack").removeClass("hidden").addClass("container");
+            console.log("House Made Blackjack, You Lose");
+        }
+}
+function hideHitStand(){
+    $("#bttn2, #bttn3").removeClass("container").addClass("hidden");
+}
 /*function which adds together the weights of an array, then checks for aces to give aces different values if necessary*/
 function addHand(array){
     let handWeight = array.map(({name : letters, weight : value}) => (value));
@@ -240,7 +331,6 @@ function addHand(array){
     return sum;
     // let handNames = array.map(({name : letters, weight : value}) => (letters));
     // let checkForAce = handNames.some(() => 'dA' || 'hA' || 'sA' || 'cA');
-    // let numAces = handNames.filter(name => (name === 'dA' || 'hA' || 'sA' || 'cA'));
     //     while (sum>21){
     //         if ((checkForAce === true) && (numAces.length = 1)){
     //             let
@@ -248,11 +338,6 @@ function addHand(array){
     //         }
     //     }
 }
-shuffle(grabDeck);
-deal(playerHand);
-deal(playerHand);
-let sumHand = addHand(playerHand);
-console.log(sumHand);
 
 
 
